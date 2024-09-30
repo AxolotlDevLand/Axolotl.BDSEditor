a8c6be9f2949bfb85635e4af8e92fc5db22c6476
YoumiHa <YoumiHa@outlook.it>
2024-10-01 00:02:15 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Axolotl.BDSEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
